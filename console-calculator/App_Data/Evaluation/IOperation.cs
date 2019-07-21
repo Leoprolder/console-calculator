f6ab92eb48ba04fca5d7d469fe7d8cfbeb5f5cab
AntonK <leopold.1996@inbox.ru>
2019-07-22 02:38:25 +0500

    Start building logics

---
+++ console-calculator/App_Data/Evaluation/IOperation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Evaluation
+{
+    interface IOperation
+    {
+        double Evaluate(double first, double second);
+
+        bool IsLegal(); //Нужен класс-определитель операции
+    }
+}
